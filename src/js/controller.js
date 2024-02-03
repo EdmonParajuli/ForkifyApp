71e645f4366c8dbbabc70c64c41edd7b0930335d
EdmonParajuli <edmonparajuli2060@gmail.com>
2024-02-03 19:38:41 +0545

    Add new console.loooooooooooooggggggggggggggg

--- src/js/controller.js
+++ src/js/controller.js
@@ -127,5 +127,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+
+  console.logggggggggggggggggggggggggggggggggggggggggggggg(asdnasdjnaskdjansdkj);
 };
 init();
