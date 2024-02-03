a579c76def6dbe0fafca444ae8d33a38ea1973fa
EdmonParajuli <edmonparajuli2060@gmail.com>
2024-02-03 20:26:07 +0545

    README.md file added

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'fd85446d-dea7-4149-b361-8d1c07722090';
 export const MODAL_CLOSE_SEC = 2.5;
